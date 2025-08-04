3f3a7a9570f53cd980b1d945f9a368c0883fa24f
Victor Brandi <victorterrenguibrand@gmail.com>
2025-08-04 18:14:18 -0300

    Obtendo um post por id

---
+++ src/main/java/org/projectspring/workshopmongo/resources/exception/PostResource.java
@@ -0,0 +1,28 @@
+package org.projectspring.workshopmongo.resources.exception;
+
+import org.projectspring.workshopmongo.domain.Post;
+import org.projectspring.workshopmongo.domain.User;
+import org.projectspring.workshopmongo.dto.UserDTO;
+import org.projectspring.workshopmongo.services.PostService;
+import org.projectspring.workshopmongo.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping(value = "/posts")
+public class PostResource {
+    @Autowired
+    private PostService service;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Post> findById(@PathVariable String id) {
+        Post obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+}
---
+++ src/main/java/org/projectspring/workshopmongo/services/PostService.java
@@ -0,0 +1,24 @@
+package org.projectspring.workshopmongo.services;
+
+import org.projectspring.workshopmongo.domain.Post;
+import org.projectspring.workshopmongo.domain.User;
+import org.projectspring.workshopmongo.dto.UserDTO;
+import org.projectspring.workshopmongo.exception.ObjectNotFoundException;
+import org.projectspring.workshopmongo.repository.PostRepository;
+import org.projectspring.workshopmongo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PostService {
+    @Autowired
+    private PostRepository repo;
+
+    public Post findById(String id) {
+        Optional<Post> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+}
