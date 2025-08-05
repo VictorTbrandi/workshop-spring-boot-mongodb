00b9c6cdb0c5e5352527a7685bce9e8229c0c965
Victor Brandi <victorterrenguibrand@gmail.com>
2025-08-05 08:47:08 -0300

    Consulta simples com query methods

--- src/main/java/org/projectspring/workshopmongo/resources/exception/PostResource.java
+++ src/main/java/org/projectspring/workshopmongo/resources/exception/PostResource.java
@@ -3,6 +3,7 @@ package org.projectspring.workshopmongo.resources.exception;
 import org.projectspring.workshopmongo.domain.Post;
 import org.projectspring.workshopmongo.domain.User;
 import org.projectspring.workshopmongo.dto.UserDTO;
+import org.projectspring.workshopmongo.resources.util.URL;
 import org.projectspring.workshopmongo.services.PostService;
 import org.projectspring.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,5 +25,12 @@ public class PostResource {
     public ResponseEntity<Post> findById(@PathVariable String id) {
         Post obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @GetMapping(value = "/titlesearch")
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+        text = URL.decodeParam(text);
+        List<Post> list = service.findByTitle(text);
+        return ResponseEntity.ok().body(list);
     }
 }
